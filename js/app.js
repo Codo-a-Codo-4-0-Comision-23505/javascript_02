04657f0239fb75f0a8234a01f279c6f2313a09be
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-10-02 20:50:02 -0300

    javascript

--- js/app.js
+++ js/app.js
@@ -42,5 +42,62 @@ myMoto.anio = "2020"
 myMoto.color = "Amarillo furioso"
 myMoto.tipo = "naked"
 
-
 console.log(myMoto.marca);
+
+var myPerson = new Object();
+
+myPerson.colorOjos = "Marrones"
+myPerson.colorPelo = "Negro"
+myPerson.numeroIdentidad = 111111111
+
+
+var socialNetwork = new Object();
+
+socialNetwork.instagram = "@nombreUsuario"
+socialNetwork.linkedin = "@UsuarioNombre"
+
+
+myPerson.mySocialNetworks = socialNetwork;
+
+var myAuto = {
+    marca: "Peugeot",
+    modelo: "206",
+    color: "negro"
+}
+
+
+console.log(myAuto.marca);
+console.log(myAuto.modelo);
+console.log(myAuto.color);
+
+
+var alumno = {
+    nombre: "Juan",
+    apellido: "Britez",
+    edad: 30,
+    comision: "23505",
+    notas: [ 1, 6, 10, 4, 3 ],
+
+    promedioNotas: function () {
+        var promedio = 0;
+
+        for (let index = 0; index < notas.length; index++) {
+            promedio += notas[index]; // notas[0]
+                                        // notas[1]
+                                        // notas[2]
+        }
+        return promedio/notas.length; // la sumatoria de las notas dividido el total...
+    }
+}
+
+console.log(alumno.promedioNotas())
+
+console.log(alumno.nombre)
+
+var myText = document.body.innerText
+
+console.log(myText)
+
+var myButton = document.getElementById("001");
+
+myButton.onclick = () => alert("Apretaron el boton")
